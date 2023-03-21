e30ea801624bdc457dca37573f3b93046cbf5ffa
Chayan Chandro Sutrodhar <109588297+ChayanSD@users.noreply.github.com>
2023-03-22 01:36:09 +0600

    Create Basics1.kt

---
+++ Basics/Basics1.kt
@@ -0,0 +1,18 @@
+ fun main(){
+     //What's the difference between var and val
+     //var is like a general variable and can be assigned multiple times and is known as the mutable variable in Kotlin.
+  // Whereas val is a constant variable and can not be assigned multiple times and can be Initialized only single time and is known as the immutable variable in Kotlin.
+    var name : String ="Chayan"
+     name ="Jhon"
+     println("My name is $name")
+
+     //another type of variable
+     var age: Int = 10;
+     println(age)
+  //Let's learn about Integer data type
+  var maxIntegerValue : Int = Int.MAX_VALUE
+  var minIntegerValue : Int= Int.MIN_VALUE
+   println(maxIntegerValue)
+  println(minIntegerValue)
+
+ }
